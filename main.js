8dbb604dedb038448cf0884a4dc5269e084ead6e
scrxwz <163136284+scrxwz@users.noreply.github.com>
2024-03-12 09:39:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="../McKenai/mckenaii.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="../McKenai/shutterstock_1806472312.jpg";
+}
+
